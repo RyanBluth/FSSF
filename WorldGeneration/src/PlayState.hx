f98c7d4187cbe57336828867742bb15b3da7e564
RyanBluth <ryanbluth93@gmail.com>
2014-06-02 19:24:23 -0400

    cleanup playstate

--- WorldGeneration/src/PlayState.hx
+++ WorldGeneration/src/PlayState.hx
@@ -45,18 +45,11 @@ import Enemy;
 import sexual_tengine.sprite.ST_Detachment;
 
 class PlayState extends ST_State{
+	
 	var bulletManager:ST_SpriteManager;
 	var scrollBackground:ST_Background;
 	var player:Player;
 	var enemies:Array<Enemy>;
-	
-	var collisionTest:ST_Sprite;
-	var debugger:ST_Console;
-	
-	var panel:ST_UiPanel;
-	var button:ST_UiButton;
-	
-	var linLay:ST_UiLinearLayout;
 	var soundManager:ST_SoundManager;
 	
 	public function new(_game:ST_Game) {
@@ -69,28 +62,14 @@ class PlayState extends ST_State{
 		bulletManager = new ST_SpriteManager(Bullet);
 		bulletManager.circleColliderRadius = 11.5;
 		
-		collisionTest = new ST_Sprite();
-		collisionTest.animation.addSpriteSheet("img/terrain.png", "main");
-		collisionTest.animation.addAnimationState("main", "main", [0], 0, 432, 216, true);
-		collisionTest.x = 700;
-		addChild(collisionTest);
-		
-		collisionTest.circleColliderRadius = 5;
-		//player.playerBody.circleColliderRadius = 5;
-		
-		//debugger = new ST_Console();
-		//addChild(debugger);
-		
 		ST_GamepadManager.addController(0);
+		
 		camera.follow(player);
 		soundManager = new ST_SoundManager();
-		soundManager.addSound("sound/laser.wav", "playerShot");
-		soundManager.addSound("sound/spaceLoop.ogg", "tunes");
-		soundManager.fadeInSound("tunes", 3000, 0, 9999);
-		
 	}
 	
-	function setupPlayer(){
+	function setupPlayer() {
+		
 		player.kinetics.friction = 0.9;
 		
 		enemies = new Array();
@@ -112,44 +91,6 @@ class PlayState extends ST_State{
 		}
 		
 		addChild(new FPS());
-		panel = new ST_UiPanel(200, 200);
-		panel.setBackgroundColour(0xFF0033, 1);
-		addChild(panel);
-		
-		button = new ST_UiButton(128, 32);
-		//panel.addComponent("button", button, 30, 30);
-		
-		button.label.text = "Button";
-		button.label.format.size = 20.5;
-		//panel.addComponent("text", new ST_UiInputText(100, 30), 50, 50);
-		var text =  new ST_UiInputText(100, 30);
-		var text2 =  new ST_UiInputText(100, 30);
-		var text3 =  new ST_UiInputText(100, 30);
-		var text4 =  new ST_UiInputText(100, 30);
-		var img = new ST_UiImage(100, 100, "img/bullet.png");
-		var label:ST_UiLabel = new ST_UiLabel(100, 100);
-		label.text.text = "Label";
-		label.text.format.size = 30;
-		panel.x = 100;
-		panel.y = 100;
-		linLay = new ST_UiLinearLayout(0, 0, VERTICAL);
-		linLay.addChildren([button,text,text2,img,text3,label,text4]);
-		panel.addComponent("lin", linLay, 50, 50);
-		text2.marginLeft = 10;
-		text2.marginRight = 10;
-		linLay.setChildOrderIndex(button, 3);
-		button.marginBottom = 20;
-		label.text.setFont("font/PTC55F.ttf");
-		
-		
-		var text5 = new ST_UiInputText(100, 30);
-		var text6 = new ST_UiInputText(100, 30);
-		
-		panel.addComponent("text5", text5, 0, 0);
-		panel.addComponent("text6", text6, 0, 0);
-		linLay.setBackgroundColour(0x0000FF, 1);
-		ST_UiLayoutUtil.center(linLay, [text5]);
-		trace(linLay.displayHeight);
 	}
 	
 	public override function update(){
@@ -173,21 +114,6 @@ class PlayState extends ST_State{
 				}
 			}
 		}
-		linLay.update();
-		panel.update();
-		button.update();
-//		trace(bulletManager.spriteArray.length,ST_Collision.spriteManagerCollide(player.playerBody, bulletManager, player).length);
-		
-		if(ST_GeneralInput.primary(0,false)){
-			//trace(ST_Collision.circleCollide(collisionTest, player.playerBody, null, player));
-			//trace(ST_Collision.checkCollision(collisionTest, player.playerBody, 0, null, player));
-			//trace(ST_Collision.checkCollision(enemies[0].enemyBody, player.playerBody, 0, enemies[0], player));
-		}
-		//if (ST_GeneralInput.primary(0,true)) {
-		//	debugger.analyze(getObjectsUnderPoint(new Point(mouseX, mouseY))[0]);
-		//}
-		//debugger.update();
-		
 	}
 	
 	private function playerMovement() {
@@ -214,9 +140,8 @@ class PlayState extends ST_State{
 			bul.x = pos.x;
 			bul.y = pos.y;
 			bul.kinetics.velocity = vel;
-			
 			addChild(bul);
-			soundManager.playSound("playerShot",0,0);
+
 		}if (ST_GeneralInput.secondary(0, false)) {
 			var pos:Point = new Point(player.x + 23*1.5, player.y - 23*2);
 			var vel:Point = new Point(player.kinetics.velocity.x*0.5*STI.corrector, Math.min(player.kinetics.velocity.y*0.5*STI.corrector - 5,-1));
@@ -225,18 +150,13 @@ class PlayState extends ST_State{
 			bul.x = pos.x;
 			bul.y = pos.y;
 			bul.kinetics.velocity = vel;
-			
 			addChild(bul);
-			soundManager.fadeOutSound("tunes", 1000);
-		
 		}
 	}
 	
 	public override function draw() {
 		super.draw();
-		linLay.draw();
 		player.draw();
-		button.draw();
 		for (e in enemies) {
 			e.draw();
 		}for (i in bulletManager.spriteArray) {
